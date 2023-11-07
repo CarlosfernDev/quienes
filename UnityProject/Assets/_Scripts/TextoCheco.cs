e844b8c032feeea77cc245cb8a25072a5c1c3f34
bo1x <alejandroboixperez@gmail.com>
2023-11-07 19:57:24 +0100

    script texto

---
+++ UnityProject/Assets/_Scripts/TextoCheco.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextoCheco : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+Homer Simpson
+Bart Simpson
+Marge Simpson
+Lisa Simpson
+Maggie Simpson
+Abraham Simpson
+Apu Nahasapeemapetilon
+Principal Skinner
+Chief Wiggum
+Krusty the clown
+Milhouse Van Houten
+Edna Krabappel
+Fat Tony
+Mayor Quimby
+Lenny Leonard
+Barney Gumble
+Carl Carlson
+Moe Szyslak
+Otto Mann
+Kent Brockman
+Reverend Lovejoy
+Martin Prince
+Comic Book Guy
+Groundskeeper Willies
+         */
+
+        /*
+¿Es hombre?
+¿Es mujer?
+¿Tiene gafas?
+¿Tiene la piel amarilla?
+¿Tiene la piel oscura?
+         */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
